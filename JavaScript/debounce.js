d85f1c42d67958b0d1c81ad47d70588f92adb66a
guanhai <q2274799107@126.com>
2024-12-18 23:26:12 +0800

    feat(add js):添加手写防抖和节流函数

---
+++ JavaScript/debounce.js
@@ -0,0 +1,72 @@
+/** 防抖
+ * n 秒后在执行该事件，若在 n 秒内被重复触发，则重新计时
+ */
+
+// 简易版防抖函数
+function baseDebounce(fn, delay) {
+  let timer = null
+  const _debounce = () => {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn()
+      timer = null
+    }, delay)
+  }
+  return _debounce
+}
+
+// 标准版防抖函数
+function _debounce(fn, delay) {
+  let timer = null
+  const _debounce = function (...args) {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+      timer = null
+    }, delay)
+  }
+  return _debounce
+}
+
+/** 扩展版防抖函数
+ * 1. 支持取消防抖，在停止高频率事件后，在0 ~ delay ms内可以取消防抖操作
+ * 2. 立即执行，在首次执行高频率事件时，立即执行一次，然后再执行防抖操作，然后随着执行高频率事件循环往复
+ * 3. 返回值，在防抖函数执行完毕后返回fn函数的结果
+ */
+function debounce(fn, delay, immediate = false) {
+  let timer = null
+  let isInvoke = false
+  const _debounce = function (...args) {
+    let result = undefined
+    return new Promise((resolve, reject) => {
+      try {
+        if (timer) clearTimeout(timer)
+        if (immediate && !isInvoke) {
+          result = fn.apply(this, args)
+          isInvoke = true
+          resolve(result)
+        }
+        timer = setTimeout(() => {
+          result = fn.apply(this, args)
+          timer = null
+          isInvoke = false
+          resolve(result)
+        }, delay)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+  // 绑定一个取消的函数
+  _debounce.cancel = function () {
+    if (timer) clearTimeout(timer)
+    timer = null
+  }
+  return _debounce
+}
+
+module.exports = {
+  baseDebounce,
+  _debounce,
+  debounce,
+}
---
+++ JavaScript/throttle.js
@@ -0,0 +1,95 @@
+/**
+ * 节流：n 秒内只运行一次，若在 n 秒内重复触发，只有一次生效
+ */
+
+/**
+ * 思路：
+ * 等待时间 = 间隔 - (当前时间 - 开始时间)
+ * if(等待时间 <= 0) {
+ *  fn()
+ *  开始时间 = 当前时间
+ * }
+ */
+
+// 基础版
+function baseThrottle(fn, interval) {
+  let startTime = 0
+  const _throttle = function () {
+    const nowTime = new Date().getTime()
+    const waitTime = interval - (nowTime - startTime)
+    if (waitTime <= 0) {
+      fn()
+      startTime = nowTime
+    }
+  }
+  return _throttle
+}
+
+// 标准版
+function _throttle(fn, interval) {
+  let startTime = 0
+  const _throttle = function (...args) {
+    const nowTime = new Date().getTime()
+    const waitTime = interval - (nowTime - startTime)
+    if (waitTime <= 0) {
+      fn.apply(this, args)
+      startTime = nowTime
+    }
+  }
+  return _throttle
+}
+
+/** 扩展版节流函数
+ * 1.对立即执行进行控制
+ *  options.leading 为 false 时，第一次执行高频率事件不会触发fn函数，且才开始倒计时，倒计时完毕才会执行fn函数
+ * 2.对尾部进行控制
+ *  options.trailing 为 true 时，会在最后一次触发高频率事件触发fn函数，怎么判断用户是不是最后一次触发这个非常不好把握
+ * 3.节流取消功能
+ */
+function throttle(fn, interval, { leading = true, trailing = false } = {}) {
+  let startTime = 0
+  let timer = null
+  let res = undefined
+  const _throttle = function (...args) {
+    return new Promise((resolve, reject) => {
+      try {
+        const nowTime = new Date().getTime()
+        // 对立即执行进行控制
+        if (!leading && startTime === 0) {
+          startTime = nowTime
+        }
+        const waitTime = interval - (nowTime - startTime)
+        if (waitTime <= 0) {
+          if (timer) clearTimeout(timer)
+          res = fn.apply(this, args)
+          startTime = nowTime
+          resolve(res)
+        }
+        // 判断是否尾部执行
+        if (trailing && !timer) {
+          timer = setTimeout(() => {
+            res = fn.apply(this, args)
+            startTime = new Date().getTime()
+            timer = null
+            resolve(res)
+          }, waitTime)
+        }
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
+  // 取消功能
+  _throttle.cancel = function () {
+    if (timer) clearTimeout(timer)
+    startTime = 0
+    timer = null
+  }
+  return _throttle
+}
+
+module.exports = {
+  baseThrottle,
+  _throttle,
+  throttle,
+}
