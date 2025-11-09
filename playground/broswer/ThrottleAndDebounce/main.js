0daadd00e054cd89ee6e9c2c5bf205d4009fd793
guanhai <q2274799107@126.com>
2025-11-09 23:08:04 +0800

    feat(playground/broswer): 为防抖节流添加案例

--- playground/broswer/ThrottleAndDebounce/main.js
+++ playground/broswer/ThrottleAndDebounce/main.js
@@ -1,22 +1,47 @@
-// import { baseDebounce, _debounce, debounce } from "../JavaScript/debounce.js"
-import { throttle } from "@code/business";
-const searchIpt = document.querySelector("#searchIpt");
-const cancelBtn = document.querySelector("#cancelBtn");
+import "./style.scss";
+import { throttle, debounce } from "@code/business";
 
-const searchEvent = throttle(
-  (event) => {
-    console.log("搜索完毕！", Math.floor(Math.random() * 9) + 1);
-    console.log("event: ", event);
-  },
-  2000,
-  {
-    leading: false,
-    trailing: true,
-  },
-);
+const [boxEl1, boxEl2] = [document.querySelector(".box-1"), document.querySelector(".box-2")];
 
-searchIpt.addEventListener("input", searchEvent);
-cancelBtn.addEventListener("click", () => {
-  console.log("cancel");
-  searchEvent.cancel();
-});
+// 防抖
+(() => {
+  const [searchInput, cancelBtn] = [
+    boxEl1.querySelector("input.searchIpt"),
+    boxEl1.querySelector("button.cancelBtn"),
+  ];
+
+  const searchEvent = debounce(
+    (event) => {
+      console.log("搜索完毕！", Math.floor(Math.random() * 9) + 1);
+      console.log("event: ", event);
+    },
+    2000,
+    {
+      leading: false,
+      trailing: true,
+    },
+  );
+
+  searchInput.addEventListener("input", searchEvent);
+  cancelBtn.addEventListener("click", () => {
+    console.log("cancel");
+    searchEvent.cancel();
+  });
+})();
+
+// 节流
+(() => {
+  const content = boxEl2.querySelector("div.content");
+  const pointSpan = boxEl2.querySelector("span.point-record");
+
+  content.addEventListener(
+    "mousemove",
+    throttle((e) => {
+      pointSpan.innerHTML = `x轴坐标为 ${e.offsetX}, y轴坐标为 ${e.offsetY}`;
+    }, 200),
+  );
+
+  content.addEventListener("mouseleave", () => {
+    pointSpan.textContent = "鼠标已离开网格区域";
+  });
+})();
